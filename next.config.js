c0da418b5ffdb399ae7fc00dacce2d117f16c5c2
Diego Q. <diiegg@users.noreply.github.com>
2026-02-08 14:26:50 +0100

    feat: add Netlify configuration with security headers

--- next.config.js
+++ next.config.js
@@ -44,6 +44,18 @@ const nextConfig = {
             key: 'Referrer-Policy',
             value: 'origin-when-cross-origin'
           },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: 'upgrade-insecure-requests'
+          },
         ],
       },
       {
